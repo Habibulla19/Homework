b035ccdfbdce0221f1e89dc5935b5363996546ad
Habibulla19 <gabibulaev1997@mail.ru>
2024-06-07 17:12:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+a = 'Khabibulla'
+print(len(a))
+
+a = 10
+b = 13
+s = a+b
+d = a-b
+print(str(s)+',' + str(d))
+
+a = 7
+b = 8
+c = 9
+s = (a+b+c)/3
+print(s)
+
+f = 'Вторник'
+s = 'Понедельник'
+print(s+ ',', f)
+print(s+ ',' +f)
+
+a = 8
+b = 11
+c = 5
+f = (a+b) * (a+c)
+n = (f**3)/2
+print(n)
+print((f**3)/2)
