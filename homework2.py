f0434064cbf00da81759e49b1de7ef937bab2523
Habibulla19 <gabibulaev1997@mail.ru>
2024-06-09 11:32:27 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,24 @@
+first_name = 'Vasya'
+apple_count = 10
+second_name = 'Petya'
+orange_count = 15
+print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
+
+a = 'Для'
+b = 'вывода'
+c = 'значений'
+d = 'на экран'
+e = 'используйте'
+f = 'функцию'
+print(a, b, c, d, e, f)
+
+first_name = 'Denis'
+banana_count = 7
+pineapple_count = 2
+second_name = 'Andrey'
+print(first_name, 'купил', banana_count, 'бананов и', pineapple_count, 'ананаса', )
+
+
+
+
+
