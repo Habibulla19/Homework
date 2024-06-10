43a4fc173aeaa6fe088279966c6d0ce710c74040
Habibulla19 <gabibulaev1997@mail.ru>
2024-06-10 15:37:42 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,22 +1,8 @@
-first_name = 'Vasya'
-apple_count = 10
-second_name = 'Petya'
-orange_count = 15
-print(first_name, 'дал', second_name, apple_count, 'яблок и', orange_count, 'апельсинов')
-
-a = 'Для'
-b = 'вывода'
-c = 'значений'
-d = 'на экран'
-e = 'используйте'
-f = 'функцию'
-print(a, b, c, d, e, f)
-
-first_name = 'Denis'
-banana_count = 7
-pineapple_count = 2
-second_name = 'Andrey'
-print(first_name, 'купил', banana_count, 'бананов и', pineapple_count, 'ананаса', )
+DZ = 12
+hours = 1.5
+name = 'python'
+time = hours / DZ
+print('Курс: '  +name, 'всего задач:' ,DZ , 'затрачено часов:' ,hours, 'среднее время выполнения' ,time, 'часа')
 
 
 
